b204e213a4de9649a6b5ae37d754127484b3ec41
Shubham Srivastava <86356397+Noobdev008@users.noreply.github.com>
2022-03-22 21:52:23 +0530

    scroll bar

---
+++ scroll_from_react/src/index.js
@@ -0,0 +1,82 @@
+import React from "react";
+import { render } from "react-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+import './index.css'
+
+const style = {
+  width: 30,
+  height: 100,
+  border: "1px solid green",
+  margin: 6,
+  padding: 8,
+};
+
+class App extends React.Component {
+  state = {
+    items: Array.from({ length: 10000 })
+  };
+
+  fetchMoreData = () => {
+    setTimeout(() => {
+      this.setState({
+        items: this.state.items.concat(Array.from({ length: 20 }))
+      });
+    }, 1500);
+  };
+
+  render() {
+    return (<>
+      <div className="container">
+        <h1>demo: react-infinite-scroll-component</h1>
+        <hr />
+        <InfiniteScroll
+          dataLength={this.state.items.length}
+          next={this.fetchMoreData}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+        >
+          {this.state.items.map((i, index) => (
+            <div style={style} key={index}>
+              div - #{index}
+            </div>
+          ))}
+        </InfiniteScroll>
+      </div>
+      <div className="container">
+        <h1>demo: react-infinite-scroll-component</h1>
+        <hr />
+        <InfiniteScroll
+          dataLength={this.state.items.length}
+          next={this.fetchMoreData}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+        >
+          {this.state.items.map((i, index) => (
+            <div style={style} key={index}>
+              div - #{index}
+            </div>
+          ))}
+        </InfiniteScroll>
+      </div>
+      <div className="container">
+        <h1>demo: react-infinite-scroll-component</h1>
+        <hr />
+        <InfiniteScroll
+          dataLength={this.state.items.length}
+          next={this.fetchMoreData}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+        >
+          {this.state.items.map((i, index) => (
+            <div style={style} key={index}>
+              div - #{index}
+            </div>
+          ))}
+        </InfiniteScroll>
+      </div>
+      </>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
